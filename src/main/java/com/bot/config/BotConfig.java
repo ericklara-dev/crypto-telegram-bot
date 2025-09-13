7b10f7d490860f41133570d1a0daca8dfff330f6
Erick <ericklararamirez9@gmail.com>
2025-09-13 13:09:14 -0600

    ...

--- src/main/java/com/bot/config/BotConfig.java
+++ src/main/java/com/bot/config/BotConfig.java
@@ -2,5 +2,5 @@ package com.bot.config;
 
 public class BotConfig {
     public static final String TOKEN = "token";
-    public static final String USERNAME = "Username";
+    public static final String USERNAME = "username";
 }
