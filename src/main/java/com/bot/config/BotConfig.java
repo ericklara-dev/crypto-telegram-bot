40a44721e9e3538c68629229ee37b865c79573c0
Erick <ericklararamirez9@gmail.com>
2025-09-14 14:12:40 -0600

    Token and username

--- src/main/java/com/bot/config/BotConfig.java
+++ src/main/java/com/bot/config/BotConfig.java
@@ -1,6 +1,9 @@
 package com.bot.config;
 
 public class BotConfig {
-    public static final String TOKEN = "token";
-    public static final String USERNAME = "username";
+    public static final String TOKEN = "token_bot";
+    public static final String USERNAME = "username_bot";
+
+    //token del bot desde botfather
+    //nombre de usuario del bot
 }
