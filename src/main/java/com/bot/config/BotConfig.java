5861d1e8377cfb6bc96bd9e6fb1e6aed994e9e3f
Erick <ericklararamirez9@gmail.com>
2025-09-18 19:28:10 -0600

    ...

--- src/main/java/com/bot/config/BotConfig.java
+++ src/main/java/com/bot/config/BotConfig.java
@@ -1,8 +1,8 @@
 package com.bot.config;
 
 public class BotConfig {
-    public static final String TOKEN = "Token";
-    public static final String USERNAME = "BitBeeperBot";
+    public static final String TOKEN = "TOKEN_AQUI";
+    public static final String USERNAME = "USERNAME_A";
 
     //token del bot desde botfather
     //nombre de usuario del bot
